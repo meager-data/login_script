de2e7a0425d2a1191d8010c8bf36518e9c328247
Josetheworm <tyler.watkins@outlook.com>
2014-04-13 03:24:06 -0400

    initial commit

---
+++ register_html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html/>
+<html>
+<head>
+	<link href="style.css" rel="stylesheet"/>
+</head>
+<body>
+	<div id="register_box">
+		<form action="register.php" method="POST">
+			<div id="username_line">Username: <input type="text" name="username" id="username"/></div>
+			<br/>
+			<div id="password_line">Password: <input type="password" name="password" id="password"/></div>
+			<br/>
+			<input type="submit" value="register" id="submit_button"/>
+		</form>
+	</div>
+</body>
+</html>
