58da5bad8759380472ee2548def238c0501900c6
Josetheworm <tyler.watkins@outlook.com>
2014-04-13 03:23:29 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'connection.php'; ?>
+<!DOCTYPE html/>
+<html>
+<head>
+	<link href="style.css" rel="stylesheet"/>
+</head>
+<body>
+	<div id="login_box">
+		<!-- PHP if/else statements added to see if user is logged in and session has been started -->
+		<?php 
+			if(isset($_SESSION['user_id'])&&!empty($_SESSION['user_id']))
+			{;
+		?>
+			<div id="welcome">Welcome, congratulations you are logged in!!</div>
+			<center><div id="link"><a href="logout.php">logout</a></div></center>
+		<?php
+		}
+		else
+		{
+		?>
+		<form action="login.php" method="POST">
+			<div id="username_line">Username: <input type="text" name="username" id="username"/></div>
+			<br/>
+			<div id="password_line">Password: <input type="password" name="password" id="password"/></div>
+			<br/>
+			<div id="reg_link"><a href="register_html.php">register</a></div><input type="submit" value="login" id="submit_button"/>
+		</form>
+		<?php
+		}
+		?>
+	</div>
+</body>
+</html>
