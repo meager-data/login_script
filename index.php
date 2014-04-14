7a13a799bbabbb2d0c4e4ad9a2436c2747d00736
Josetheworm <tyler.watkins@outlook.com>
2014-04-14 18:37:07 -0400

    Added get username function for when logged in

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 		<!-- PHP if/else statements added to see if user is logged in and session has been started -->
 		<?php 
 			if(isset($_SESSION['user_id'])&&!empty($_SESSION['user_id']))
-			{;
+			{ include 'functions.php';
 		?>
-			<div id="welcome">Welcome, congratulations you are logged in!!</div>
+			<div id="welcome">Welcome <?php echo $_SESSION['username'];?>, congratulations you are logged in!!</div>
 			<center><div id="link"><a href="logout.php">logout</a></div></center>
 		<?php
 		}
