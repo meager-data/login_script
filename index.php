e8187c7250a4ed61a210e68010d29318cf6651c0
Josetheworm <tyler.watkins@outlook.com>
2014-04-15 01:05:34 -0400

    Update

--- index.php
+++ index.php
@@ -9,15 +9,18 @@
 		<!-- PHP if/else statements added to see if user is logged in and session has been started -->
 		<?php 
 			if(isset($_SESSION['user_id'])&&!empty($_SESSION['user_id']))
-			{ include 'functions.php';
+			{ 
+			//This includes the functions.php page allowing you to access the function to retrieve the username.
+			include 'functions.php';
 		?>
-			<div id="welcome">Welcome <?php echo $_SESSION['username'];?>, congratulations you are logged in!!</div>
+			<div id="welcome">Welcome <?php /* The following line of code echos out the logged in users username*/ echo $_SESSION['username'];?>, congratulations you are logged in!!</div>
 			<center><div id="link"><a href="logout.php">logout</a></div></center>
 		<?php
 		}
 		else
 		{
 		?>
+		<!-- If not logged in and no session is started, then this form is displayed.-->
 		<form action="login.php" method="POST">
 			<div id="username_line">Username: <input type="text" name="username" id="username"/></div>
 			<br/>
